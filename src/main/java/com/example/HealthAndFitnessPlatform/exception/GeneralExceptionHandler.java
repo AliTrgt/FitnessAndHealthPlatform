09dcbcc5cd938bfe2805d14fd384b7f7a33c0990
AliTrgt <alitrgt@outlook.com>
2025-02-16 20:01:43 +0300

    writing exception code in GeneralExceptionHandler class

--- src/main/java/com/example/HealthAndFitnessPlatform/exception/GeneralExceptionHandler.java
+++ src/main/java/com/example/HealthAndFitnessPlatform/exception/GeneralExceptionHandler.java
@@ -1,7 +1,23 @@
 package com.example.HealthAndFitnessPlatform.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GeneralExceptionHandler {
+
+        @ExceptionHandler(UserNotFoundException.class)
+        public ResponseEntity<?> handleUser(){
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        @ExceptionHandler(CommentNotFoundException.class)
+        public ResponseEntity<?> handleComment(){
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+
+
 }
