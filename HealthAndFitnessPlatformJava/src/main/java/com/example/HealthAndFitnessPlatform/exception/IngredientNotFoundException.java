8e5787ea0198ab6ceeae2475f825b41333e0310c
AliTrgt <alitrgt@outlook.com>
2025-05-17 13:06:44 +0300

    .git error

--- HealthAndFitnessPlatformJava/src/main/java/com/example/HealthAndFitnessPlatform/exception/IngredientNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.HealthAndFitnessPlatform.exception;
-
-public class IngredientNotFoundException extends RuntimeException {
-
-        public IngredientNotFoundException(String msg){
-            super(msg);
-        }
-}
--- HealthAndFitnessPlatformJava/src/main/java/com/example/HealthAndFitnessPlatform/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.HealthAndFitnessPlatform.repository;
-
-import com.example.HealthAndFitnessPlatform.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Integer> {
-
-    Role findByRole(String role);
-}
