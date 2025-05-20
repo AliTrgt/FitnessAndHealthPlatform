2c5289fa88c0f6d3180aeb57127c1219dc68e463
AliTrgt <alitrgt@outlook.com>
2025-05-20 18:54:25 +0300

    designin to recommendation page

--- HealthAndFitnessPLatformNG/src/app/components/recommend/recommend.component.ts
+++ HealthAndFitnessPLatformNG/src/app/components/recommend/recommend.component.ts
@@ -12,39 +12,44 @@ import { error } from 'console';
 @Component({
   selector: 'app-recommend',
   standalone: true,
-  imports: [CommonModule,FormsModule,RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './recommend.component.html',
   styleUrl: './recommend.component.css'
 })
 export class RecommendComponent implements OnInit {
 
-  user!:User;
-  recipeList:Recipe[] = [];
-  errorMessage:string = '';
-  constructor(private recipeService:RecipeService){}
+  user!: User;
+  recipeList: Recipe[] = [];
+  errorMessage: string = '';
+  isLoading: boolean = false;
+
+  constructor(private recipeService: RecipeService) {}
+
   ngOnInit() {
-        const stringToken = localStorage.getItem('currentUser');
-        if(stringToken){
-            this.user = JSON.parse(stringToken);
-        }
-  }
-
-  getReccomendation(){
-      this.recipeService.getRecommendation(this.user.id).subscribe(response => {
-            this.recipeList = response;
-            console.log(response);
-            this.errorMessage = '';
-      },
-    (error)  => {
-          console.log('Api error');
-          this.errorMessage = 'En az 3-5 post beğenip favorilemelisin.';
+    const stringToken = localStorage.getItem('currentUser');
+    if (stringToken) {
+      this.user = JSON.parse(stringToken);
+      // Sayfa yüklendiğinde önerileri otomatik getir
+      this.getReccomendation();
     }
-  
-  )
   }
 
-
-  
-
-
+  getReccomendation() {
+    this.isLoading = true;
+    this.errorMessage = '';
+    
+    this.recipeService.getRecommendation(this.user.id).subscribe(
+      response => {
+        this.recipeList = response;
+        console.log(response);
+        this.errorMessage = '';
+        this.isLoading = false;
+      },
+      (error) => {
+        console.log('Api error', error);
+        this.errorMessage = 'En az 3-5 post beğenip favorilemelisin.';
+        this.isLoading = false;
+      }
+    );
+  }
 }
